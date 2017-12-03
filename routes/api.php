dc47b31842fce0a7ff612e25e5b265c9670cc871
Jeff <jeffer.8a@gmail.com>
2017-12-03 19:22:35 -0300

    dredd demo

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('post', function () {
+    return response()->json([
+        [
+            "id" => 1,
+            "title" => "foo title",
+            "body" => "bar body",
+            "author" => 1
+        ]
+    ]);
+});
+
+Route::get('author', function () {
+    return response()->json([
+        [
+            "id" => 1,
+            "name" => "jhon",
+            "email" => "jhon@mail.com"
+        ]
+    ]);
+});
